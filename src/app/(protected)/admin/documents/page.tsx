9d990369b9f5475f756b063cb84fffced9d84249
Dawit Negash <dawitnegash02@gmail.com>
2026-02-13 01:24:32 +0300

    chore: standardize project structure and clean legacy files

--- src/app/(protected)/admin/documents/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AdminDocumentsPage() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Document Management</h1>
-      <p className="mt-4">In Batch 2, you will be able to upload PDFs here.</p>
-    </div>
-  )
-}
